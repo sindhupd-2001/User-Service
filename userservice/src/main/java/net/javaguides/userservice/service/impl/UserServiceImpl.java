ad152222f777e7ecd365f714f2a38ea72710afd6
sindhupd-2001 <98105619+sindhupd-2001@users.noreply.github.com>
2022-11-24 07:46:47 +0530

    Initial commit

---
+++ userservice/src/main/java/net/javaguides/userservice/service/impl/UserServiceImpl.java
@@ -0,0 +1,55 @@
+package net.javaguides.userservice.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.userservice.dto.DepartmentDto;
+import net.javaguides.userservice.dto.ResponseDto;
+import net.javaguides.userservice.dto.UserDto;
+import net.javaguides.userservice.entity.User;
+import net.javaguides.userservice.repository.UserRepository;
+import net.javaguides.userservice.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+    private RestTemplate restTemplate;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public ResponseDto getUser(Long userId) {
+
+        ResponseDto responseDto = new ResponseDto();
+        User user = userRepository.findById(userId).get();
+        UserDto userDto = mapToUser(user);
+
+        ResponseEntity<DepartmentDto> responseEntity = restTemplate
+                .getForEntity("http://localhost:8080/api/departments/" + user.getDepartmentId(),
+                DepartmentDto.class);
+
+        DepartmentDto departmentDto = responseEntity.getBody();
+
+        System.out.println(responseEntity.getStatusCode());
+
+        responseDto.setUser(userDto);
+        responseDto.setDepartment(departmentDto);
+
+        return responseDto;
+    }
+
+    private UserDto mapToUser(User user){
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setFirstName(user.getFirstName());
+        userDto.setLastName(user.getLastName());
+        userDto.setEmail(user.getEmail());
+        return userDto;
+    }
+}
